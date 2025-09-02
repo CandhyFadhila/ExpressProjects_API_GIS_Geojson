af2b9a339a47ed085bc78a16a5a2b30c865682cc
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-09-02 14:00:59 +0700

    FIXED: Refactor -lco FID when creating SHP data

--- src/helpers/ogrHelper.js
+++ src/helpers/ogrHelper.js
@@ -16,11 +16,11 @@ function getOgrConfigByEnv(
 
   if (env === "linux") {
     const ogrPath = "ogr2ogr";
-    const ogrCmd = `${ogrPath} -f "PostgreSQL" PG:"host=localhost user=gisuser dbname=gisdb password=password_kuat port=5432" "${shpFilePath}" -nln "${schemaName}"."${tableName}" -nlt ${nlt} -lco GEOMETRY_NAME=geom -lco FID=gid -lco LAUNDER=NO -overwrite -t_srs EPSG:4326`;
+    const ogrCmd = `${ogrPath} -f "PostgreSQL" PG:"host=localhost user=gisuser dbname=gisdb password=password_kuat port=5432" "${shpFilePath}" -nln "${schemaName}"."${tableName}" -nlt ${nlt} -lco GEOMETRY_NAME=geom -lco FID=id -lco LAUNDER=NO -overwrite -t_srs EPSG:4326`;
     return { ogrPath, ogrCmd, env: process.env };
   } else {
     const ogrPath = `"C:\\Program Files\\QGIS 3.44.0\\bin\\ogr2ogr.exe"`;
-    const ogrCmd = `${ogrPath} -f "PostgreSQL" PG:"host=localhost user=postgres dbname=gis_bpn_v2 password=super.admin port=5433" "${shpFilePath}" -nln "${schemaName}"."${tableName}" -nlt ${nlt} -lco GEOMETRY_NAME=geom -lco FID=gid -lco LAUNDER=NO -overwrite -t_srs EPSG:4326`;
+    const ogrCmd = `${ogrPath} -f "PostgreSQL" PG:"host=localhost user=postgres dbname=gis_bpn_v2 password=super.admin port=5433" "${shpFilePath}" -nln "${schemaName}"."${tableName}" -nlt ${nlt} -lco GEOMETRY_NAME=geom -lco FID=id -lco LAUNDER=NO -overwrite -t_srs EPSG:4326`;
     return { ogrPath, ogrCmd, env: { ...process.env, ...baseEnvWin } };
   }
 }
